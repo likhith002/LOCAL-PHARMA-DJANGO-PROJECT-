eef283fb82a31660425aba192e2cb058df15227f
lokesh510 <sivasai510@gmail.com>
2021-03-19 16:11:16 +0530

    adding django registartion

--- views.py
+++ views.py
@@ -25,4 +25,76 @@ def commonloginpage(request):
 def services(request):
     return render(request,'services.html')
 
+def reegister_pharmacy(request):
+
+    if request.method == "POST":
+
+        firstname = request.POST['firstname']
+        middlename=request.POST['middlename']
+        lastname=request.POST['lastname']
+        email=request.POST['email']
+        gender=request.POST.get('gender',"Male")
+        
+        DOB=request.POST['DOB']
+        phone1=request.POST['phone1']
+        phone2=request.POST['phone2']
+        password=request.POST['password']
+        city=request.POST['city']
+        streetno=request.POST['streetno']
+        state=request.POST['state']
+        country=request.POST['country']
+        pincode=request.POST['pincode']
+        shopname=request.POST['shopname']
+
+        if pharmacyowner.objects.filter(email=email).exists():
+            messages.info(request,"Email alredy exists please provide another Email")
+            print("Email alredy exists please provide another Email")
+            return redirect('/reegister_pharmacy.html')
+
+
+        else:
+
+            obj=pharmacyowner(firstname=firstname, gender=gender,middlename=middlename,lastname=lastname,email=email,DOB=DOB,phone1=phone1,phone2=phone2,password=password,city=city,streetno=streetno,state=state,country=country,pincode=pincode,shopname=shopname)
+            obj.save()
+            print("data has been written to DB Successfully")
+            return redirect('/pharmacy_login.html')
+    else:
+        return render(request,'reegister_pharmacy.html')
+
+
+def register_customer(request):
+
+
+    if request.method == "POST":
+
+
+        firstname = request.POST['firstname']
+        middlename=request.POST['middlename']
+        lastname=request.POST['lastname']
+        email=request.POST['email']
+        gender=request.POST.get('gender',"Male")
+        
+        DOB=request.POST['DOB']
+        phone1=request.POST['phone1']
+        phone2=request.POST['phone2']
+        password=request.POST['password']
+        city=request.POST['city']
+        streetno=request.POST['streetno']
+        state=request.POST['state']
+        country=request.POST['country']
+        pincode=request.POST['pincode']
+
+        if customer.objects.filter(email=email).exists():
+            messages.info(request,"Email already exists please provide another Email")
+            return redirect('/register_customer.html')
+
+        else:
+            obj=customer(firstname=firstname, gender=gender,middlename=middlename,lastname=lastname,email=email,DOB=DOB,phone1=phone1,phone2=phone2,password=password,city=city,streetno=streetno,state=state,country=country,pincode=pincode)
+            obj.save()
+            print("data has been written to DB Successfully")
+            return  redirect('/customer_login.html')
+    else:
+        return render(request,'register_customer.html')
+
+
   
